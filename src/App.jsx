35e9e219ebbc1b682fbf3eeaddf0db7f1e40752f
Harish Kolla <harishkolla11@gmail.com>
2025-07-14 23:58:15 +0530

    fixed bug in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Tabs from './components/Tabs.jsx'
-import BrowserNavBar from './components/BrowserNavbar.jsx'
+import BrowserNavBar from './components/BrowserNavBar.jsx'
 import MainContent from './components/MainContent.jsx';
 import ThemeToggle from './components/ThemeToggle.jsx';
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [theme, setTheme] = useState("light");
+  const [activeTab, setActiveTab] = useState(null);
 
   return (
     <div className="h-screen flex flex-col">
